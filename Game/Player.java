95c3a0fa487997baa9ca17ed191560ea47c04045
staveelxd <staveel@mail.ru>
2025-03-29 21:17:22 +0300

    First mini game on Java

---
+++ Game/Player.java
@@ -0,0 +1,31 @@
+package Game;
+
+public class Player
+    private int hp;
+    private String name;
+
+    public Player(String name) {
+        this.hp = 100;
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void minusHp(int damage) {
+        this.hp -= damage * 10;
+    }
+
+    public boolean isAlive() {
+        return this.hp > 0;
+    }
+}
