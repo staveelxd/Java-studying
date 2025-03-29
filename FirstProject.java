f59d591b96d49dcb6c328be3e257c0ca31037a4b
staveelxd <staveel@mail.ru>
2025-03-29 19:44:11 +0300

    First project on Java

--- FirstProject.java
+++ FirstProject.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class FirstProject {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i = 1; i < n+1; i++) {
+            String spc = " ".repeat(n-i);
+            String a = "1 ".repeat(n);
+            System.out.println(spc + a);
+        }
     }
 }
